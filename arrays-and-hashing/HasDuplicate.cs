1c59d3ede25822afbc9c53da7269fbd95c293057
Jonah Morgan <github.finished102@passinbox.com>
2025-11-30 21:28:47 -0600

    COMPLETED: hasDuplicate

--- arrays-and-hashing/HasDuplicate.cs
+++ arrays-and-hashing/HasDuplicate.cs
@@ -1,1 +1,21 @@
-Console.WriteLine("Hello");
+using System.Diagnostics;
+
+bool hasDuplicate(int[] nums)
+{
+    var numSet = new HashSet<int>();
+    foreach(var num in nums)
+    {
+        if(numSet.Contains(num))
+        {
+            return true;
+        }
+        numSet.Add(num);
+    }
+    return false;
+}
+
+Debug.Assert(hasDuplicate([1, 2, 2, 3]) == true);
+Debug.Assert(hasDuplicate([]) == false);
+Debug.Assert(hasDuplicate([1, 2, 3]) == false);
+Debug.Assert(hasDuplicate([-1, -1, 2, 3]) == true);
+Debug.Assert(hasDuplicate([-1, -1, 2, 3, 3]) == true);
