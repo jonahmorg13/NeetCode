d09c649a742b8edff26a2feb1d53772f5c8ad3bf
Jonah Morgan <github.finished102@passinbox.com>
2025-12-04 10:59:31 -0600

    started working on valid palindrome

---
+++ two-pointers/ValidPalindrome.cs
@@ -0,0 +1,9 @@
+using System.Diagnostics;
+
+bool IsPalindrome(string s)
+{
+    return false;
+}
+
+Debug.Assert(IsPalindrome("Was it a car or a cat I saw?"));
+Debug.Assert(!IsPalindrome("tab a cat"));
