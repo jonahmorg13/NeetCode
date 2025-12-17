0a8872c9c2d93b4c0e6f667b0bddfe6ca4682ced
Jonah Morgan <github.finished102@passinbox.com>
2025-12-17 16:08:40 -0600

    another solution for max binary tree depth implemented

--- trees/MaximumDepthOfBinaryTree.cs
+++ trees/MaximumDepthOfBinaryTree.cs
@@ -13,17 +13,49 @@ public class TreeNode
 
 public class Solution
 {
+    // can do this many ways! recursivly using DFS. or using a queue BFS.
+    // could we use a stack and DFS?
+    // yes, you can store a tuple of the node itself associated with it's calcualted depth
+    // would be useful if we needed to know the depth of each node :0
+
+    // public int MaxDepth(TreeNode root)
+    // {
+    //     var depth = DFS(root);
+    //     return depth;
+    // }
+
+    // private int DFS(TreeNode node)
+    // {
+    //     if(node == null) return 0;
+    //     var left = DFS(node.left);
+    //     var right = DFS(node.right);
+    //     return 1 + Math.Max(left, right);
+    // }
+
     public int MaxDepth(TreeNode root)
     {
-        var depth = DFS(root);
-        return depth;
-    }
+        Queue<TreeNode> q = new Queue<TreeNode>();
+        if(root != null)
+            q.Enqueue(root);
 
-    private int DFS(TreeNode node)
-    {
-        if(node == null) return 0;
-        var left = DFS(node.left);
-        var right = DFS(node.right);
-        return 1 + Math.Max(left, right);
+        int level = 0;
+        while(q.Count > 0)
+        {
+            int size = q.Count;
+            for(int i = 0; i < size; i++)
+            {
+                TreeNode node = q.Dequeue();
+                if(node.left != null)
+                {
+                    q.Enqueue(node.left);
+                }
+                if(node.right != null)
+                {
+                    q.Enqueue(node.right);
+                }
+            }
+            level++;
+        }
+        return level;
     }
 }
