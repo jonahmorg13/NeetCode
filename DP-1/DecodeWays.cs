c192a3f75de8c8dbc2f4ddaf9f662dfa4e33571d
Jonah Morgan <github.finished102@passinbox.com>
2026-01-23 09:06:19 -0600

    COMPLETED: decode ways

---
+++ DP-1/DecodeWays.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.AccessControl;
+
+var sol = new Solution();
+var res = sol.NumDecodings("12");
+Debug.Assert(res == 2);
+
+public class Solution {
+    public int NumDecodings(string s)
+    {
+        int Dfs(int i)
+        {
+            if(i == s.Length) return 1;
+            if(s[i] == '0') return 0;
+
+            int res = Dfs(i + 1);
+            if(i < s.Length - 1)
+            {
+                if(s[i] == '1' ||
+                (s[i] == '2' && s[i + 1] == '7'))
+                {
+                    res += Dfs(i + 2);
+                }
+            }
+            return res;
+        }
+        
+        return Dfs(0);
+    }
+}
