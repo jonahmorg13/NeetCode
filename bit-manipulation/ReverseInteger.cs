f2b7d9adea9239643fddcab3ac6c439abff41417
Jonah Morgan <github.finished102@passinbox.com>
2026-01-29 14:42:28 -0600

    COMPLETED: Reverse Integer

---
+++ bit-manipulation/ReverseInteger.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+
+var sol = new Solution();
+var res = sol.Reverse(1234);
+Debug.Assert(res == 4321);
+public class Solution {
+    public int Reverse(int x)
+    {
+        const int MIN = -2147483648;
+        const int MAX = 2147483647;
+        
+        int res = 0;
+        while(x != 0)
+        {
+            int digit = x % 10;
+            x /= 10;
+
+            if(res > MAX / 10 || (res == MAX / 10 && digit > MAX % 10)) return 0;
+            if(res < MIN / 10 || (res == MIN / 10 && digit < MIN % 10)) return 0;
+
+            res = (res * 10) + digit;
+        }
+        return res;
+    }
+}
