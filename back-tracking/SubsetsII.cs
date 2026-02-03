6a25dae03c540c9b5bb22b219191432a6cbc2c91
Jonah Morgan <github.finished102@passinbox.com>
2026-02-03 08:30:10 -0600

    COMPLETED: Subsets II

--- back-tracking/SubsetsII.cs
+++ back-tracking/SubsetsII.cs
@@ -5,44 +5,37 @@ var res = sol.SubsetsWithDup([1,2,1]);
 return 0;
 
 public class Solution {
-    public class ListComparer : IEqualityComparer<List<int>>
+    public List<List<int>> SubsetsWithDup(int[] nums)
     {
-        public bool Equals(List<int> x, List<int> y)
-        {
-            if (x == null || y == null) return false;
-            return x.SequenceEqual(y);
-        }
+        var sol = new List<List<int>>();
+        var currSeq = new List<int>();
+        dfs(nums, 0, currSeq, sol);
+        return sol;
+    }
 
-        public int GetHashCode(List<int> obj)
+    private void dfs(int[] nums, int i, List<int> currSeq, List<List<int>> sol)
+    {
+        // handle duplicates here
+        var seq = currSeq.OrderBy(x => x);
+        var found = false;
+        foreach(var s in sol)
         {
-            if (obj == null) return 0;
-            int hash = 17;
-            foreach (var item in obj)
+            if(seq.SequenceEqual(s))
             {
-                hash = hash * 31 + item.GetHashCode();
+                found = true;
             }
-            return hash;
         }
-    }
-
-    public List<List<int>> SubsetsWithDup(int[] nums) {
-        var sets = new HashSet<List<int>>(new ListComparer());
-        var subset = new List<int>();
-        dfs(nums, 0, subset, ref sets);
-        return sets.ToList();
-    }
-
-    private void dfs(int[] nums, int i, List<int> subset, ref HashSet<List<int>> sets)
-    {
-        var newSubset = new List<int>(subset);
-        if(!sets.Contains(newSubset))
-            sets.Add(newSubset);
+        if(!found) sol.Add(new List<int>(seq.OrderBy(x => x)));
 
         if(i >= nums.Length)
+        {
             return;
+        }
 
-        newSubset.Add(nums[i]);
-        dfs(nums, i + 1, newSubset, ref sets);
-        dfs(nums, i + 2, newSubset, ref sets);
+        currSeq.Add(nums[i]);
+        dfs(nums, i + 1, currSeq, sol);
+        currSeq.RemoveAt(currSeq.Count - 1);
+        dfs(nums, i + 1, currSeq, sol);
     }
+
 }
