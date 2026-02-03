1d25726fd29eeab0503bc62e591487a3ba66afd2
Jonah Morgan <github.finished102@passinbox.com>
2026-02-03 08:35:56 -0600

    better runtime for subsetsII

--- back-tracking/SubsetsII.cs
+++ back-tracking/SubsetsII.cs
@@ -9,32 +9,27 @@ public class Solution {
     {
         var sol = new List<List<int>>();
         var currSeq = new List<int>();
+        Array.Sort(nums);
         dfs(nums, 0, currSeq, sol);
         return sol;
     }
 
     private void dfs(int[] nums, int i, List<int> currSeq, List<List<int>> sol)
     {
-        // handle duplicates here
-        var seq = currSeq.OrderBy(x => x);
-        var found = false;
-        foreach(var s in sol)
-        {
-            if(seq.SequenceEqual(s))
-            {
-                found = true;
-            }
-        }
-        if(!found) sol.Add(new List<int>(seq.OrderBy(x => x)));
-
         if(i >= nums.Length)
         {
+            sol.Add(new List<int>(currSeq));
             return;
         }
 
         currSeq.Add(nums[i]);
         dfs(nums, i + 1, currSeq, sol);
         currSeq.RemoveAt(currSeq.Count - 1);
+
+        // this is how we handle duplicates
+        while(i + 1 < nums.Length && nums[i] == nums[i + 1])
+            i++;
+
         dfs(nums, i + 1, currSeq, sol);
     }
 
