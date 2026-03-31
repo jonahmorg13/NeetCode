f8873254c8e8ef335bfbf48b32cc993791590cc2
Jonah Morgan <github.finished102@passinbox.com>
2026-03-31 08:57:10 -0500

    refactored partition label

--- greedy/PartitionLabels.cs
+++ greedy/PartitionLabels.cs
@@ -9,29 +9,22 @@ public class Solution
 {
     public List<int> PartitionLabels(string s)
     {
-        var firstAppearances = new Dictionary<char, int>();
-        var intervals = new List<(int, int)>();
-
+        var lastIndex = new int[26];
         for(int i = 0; i < s.Length; i++)
-        {
-            var currChar = s[i];
-            if(!firstAppearances.ContainsKey(currChar))
-            {
-                firstAppearances[currChar] = i;
-                intervals.Add((i, i));
-                continue;        
-            }
-
-            // we have a first appearance. lets update our intervals!
-            var charFirstIndex = firstAppearances[currChar];
-            while(i > intervals[intervals.Count - 1].Item2 && charFirstIndex < intervals[intervals.Count  - 1].Item1)
-                intervals.RemoveAt(intervals.Count - 1);
-            intervals[intervals.Count - 1] = (intervals[intervals.Count - 1].Item1, i);
-        }
+            lastIndex[s[i] - 'a'] = i;
 
         var res = new List<int>();
-        for(int i = 0; i < intervals.Count; i++)
-            res.Add(intervals[i].Item2 - intervals[i].Item1 + 1);
+        int start = 0, end = 0;
+        for(int i = 0; i < s.Length; i++)
+        {
+            end = Math.Max(end, lastIndex[s[i] - 'a']);
+            if(i == end)
+            {
+                res.Add(end - start + 1);
+                start = i + 1;
+            }
+        }
+
         return res;
     }
 }
