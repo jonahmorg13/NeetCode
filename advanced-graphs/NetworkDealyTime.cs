364cdb692bd9237b17506aaeeaad808bab043bd7
Jonah Morgan <github.finished102@passinbox.com>
2026-04-09 09:01:42 -0500

    completed: network delay time

--- advanced-graphs/NetworkDealyTime.cs
+++ advanced-graphs/NetworkDealyTime.cs
@@ -11,35 +11,56 @@ public class Solution
 {
     public int NetworkDelayTime(int[][] times, int n, int k)
     {
-        var edges = new Dictionary<int, PriorityQueue<int, int>>();
+        // we need to make our graph
+        // int1 = node, List<(int, int)> a list of destinations associated with their time
+        var edges = new Dictionary<int, List<(int, int)>>();
         for(int i = 0; i < times.Length; i++)
         {
             var src = times[i][0];
             var dst = times[i][1];
             var time = times[i][2];
             if(!edges.ContainsKey(src))
-                edges[src] = new PriorityQueue<int, int>(Comparer<int>.Create((a, b) => b.CompareTo(a)));
-            edges[src].Enqueue(dst, time);
+                edges[src] = new List<(int, int)>();
+            edges[src].Add((dst, time));
         }
-        var visited = new HashSet<int>();
-        var queue = new Queue<(int, int)>();
-        queue.Enqueue((k, 0));
-        var res = 0;
-        while(queue.Count > 0)
+
+        //make sure to properly index in the near future!
+        var distances = new int[n];
+        var visited = new bool[n];
+        for(int i = 0; i < distances.Length; i++)
+            distances[i] = Int32.MaxValue;
+        distances[k - 1] = 0;
+
+        var q = new PriorityQueue<int, int>();
+        q.Enqueue(k, 0);
+        while(q.TryDequeue(out var node, out var time))
         {
-            var (currNode, currTime) = queue.Dequeue();
-            if(!edges.ContainsKey(currNode)) continue;
-            var currEdges = edges[currNode];
-            var amtCurrEdges = currEdges.Count;
-            for(int i = 0; i < amtCurrEdges; i++)
+            if(visited[node - 1]) continue;
+            visited[node - 1] = true;
+
+            if(!edges.ContainsKey(node)) continue;
+            var nodeEdges = edges[node];
+            for(int i = 0; i < nodeEdges.Count; i++)
             {
-                currEdges.TryDequeue(out var next, out int time);
-                if(visited.Contains(next)) continue;
-                res += time;
-                visited.Add(next);
-                queue.Enqueue((next, currTime + time));
+                var nextNode = nodeEdges[i].Item1;
+                var timeToNextNode = nodeEdges[i].Item2;
+                // if the time plus the next time to the next node is less than the currently saved dist
+                if(time + timeToNextNode < distances[nextNode - 1])
+                {
+                    //enqueue, update
+                    distances[nextNode - 1] = time + timeToNextNode;
+                    q.Enqueue(nextNode, time + timeToNextNode);
+                }
             }
         }
+
+        int res = Int32.MinValue;
+        for(int i = 0; i < distances.Length; i++)
+        {
+            if(distances[i] == Int32.MaxValue) return -1;
+            res = Math.Max(res, distances[i]);
+        }
+
         return res;
     }
 }
